ce34f7e254461c1caae2ce04a2fb0e2b1469d90e
nazex2000 <nazariowizz@gmail.com>
2025-04-17 13:08:33 +0200

    Correcoes

--- src/components/about/description.section.js
+++ src/components/about/description.section.js
@@ -48,6 +48,7 @@ export default function Description() {
                                     />
                                 </div>
                             </div>
+                            
                         </div>
                     </RevealOnScroll>
 
